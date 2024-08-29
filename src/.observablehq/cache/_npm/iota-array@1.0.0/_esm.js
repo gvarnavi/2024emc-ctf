a43cb84981eea2547820c4e99f8a95c30b4800e2
gvarnavi <gvarnavi@mit.edu>
2024-08-29 10:25:25 +0200

    oops, wrong gitignore

--- src/.observablehq/cache/_npm/iota-array@1.0.0/_esm.js
+++
@@ -1,7 +0,0 @@
-/**
- * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
- * Original file: /npm/iota-array@1.0.0/iota.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-var r=function(r){for(var a=new Array(r),e=0;e<r;++e)a[e]=e;return a};export{r as default};
